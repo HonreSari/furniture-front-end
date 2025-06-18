2eadf1ef33aeff447a239213587fcc4b892e3ba2
Thar Luke <gyit7159@gmail.com>
2025-06-18 22:33:28 +0630

    changes

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,10 @@
+import { Icons } from "@/Components/icons";
+import Footer from "@/Components/layouts/Footer";
 import Header from "@/Components/layouts/Header";
 import { Button } from "@/Components/ui/button";
 
 import {
   Card,
-  CardAction,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -12,22 +13,25 @@ import {
 import { Link } from "react-router-dom";
 export default function Error() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <div className="flex flex-1 mt-20 items-center justify-center">
-        <Card className="md:w-[200px] lg:w-[400px]">
-          <CardHeader>
-            <CardTitle className="text-3xl lg:text-5xl text-center font-bold">404!</CardTitle>
-            <CardDescription className="text-center">An error occurs Accidently</CardDescription>
-            <CardAction></CardAction>
+      <main className="mx-auto flex flex-1 items-center">
+        <Card className="w-[350px] md:w-[200px] lg:w-[400px]">
+          <CardHeader className="flex place-items-center gap-2">
+            <div className="border-muted-foreground/70 mt-2 mb-4 grid size-24 place-items-center rounded-full border border-dashed">
+              <Icons.exclamation className="size-10 text-red-600" />
+            </div>
+            <CardTitle>404!</CardTitle>
+            <CardDescription>An error occurs Accidently</CardDescription>
           </CardHeader>
-          <CardFooter  className="flex justify-center" >
-            <Link to="/">
-            <Button variant="outline">Click Here!</Button>
-            </Link>
+          <CardFooter className="flex justify-center">
+            <Button variant="outline" asChild>
+              <Link to="/">Click Here</Link>
+            </Button>
           </CardFooter>
         </Card>
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 }
